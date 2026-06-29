7b7759e5586eb6cb3f68c3e84c991d9b23264ab1
Nisan Katalan <nisank2@gmail.com>
2026-06-29 17:29:08 +0300

    ..

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
+// משיכת הנתונים המאובטחים שהגדרנו בקובץ ה-env.
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
+// יצירת הצינור החי מול מסד הנתונים
 export const supabase = createClient(supabaseUrl, supabaseKey);
