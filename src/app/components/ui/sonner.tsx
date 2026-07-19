3566349398fce0d348e903172fe21dd92e515019
Nisan Katalan <nisank2@gmail.com>
2026-07-19 20:38:15 +0300

    .

--- src/app/components/ui/sonner.tsx
+++ src/app/components/ui/sonner.tsx
@@ -9,6 +9,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
+      dir="rtl"
       className="toaster group"
       style={
         {
